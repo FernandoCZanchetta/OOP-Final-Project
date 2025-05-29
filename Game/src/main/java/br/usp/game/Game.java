f182e5a085126527b183e640d12b5eaa0313790a
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2025-05-29 07:33:06 -0300

    Add the entry point for the Game (main)

---
+++ Game/src/main/java/br/usp/game/Game.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.usp.game;
+
+import br.usp.core.GameEngine;
+import br.usp.view.SpriteManager;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            GameEngine game = GameEngine.getGameEngineInstance();
+            
+            game.loadSprites();
+            
+            game.run();  
+        });
+    }
+}
