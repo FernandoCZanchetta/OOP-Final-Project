cd73f459435acc50a50f9eae12dd122d136e3fee
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2025-05-29 07:37:38 -0300

    Add some usefull clases in utils to abstract some concepts

---
+++ Game/src/main/java/br/usp/util/GameConstants.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.usp.util;
+
+/**
+ *
+ * @author Fernando
+ */
+public final class GameConstants {
+    private GameConstants() {};
+    
+    // Limites do mundo
+    public static final int MAX_MAP_WIDTH = 100;
+    public static final int MAX_MAP_HEIGHT = 100;
+
+    // Tamanho do tile
+    public static final int TILE_SIZE = 64;
+
+    // Tamanho da janela
+    public static final int WINDOW_WIDTH = 960;
+    public static final int WINDOW_HEIGHT = 720;
+
+    // Taxa FPS
+    public static final int GAME_FPS = 60;
+    
+    // Player
+    public static final int HERO_MAX_HP = 5;
+}
