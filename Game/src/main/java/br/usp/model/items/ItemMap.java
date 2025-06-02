10a91a908981844f383aac3ace3235e69b326b5d
Fernando Cirilo Zanchetta <fe.cz@usp.br>
2025-06-02 02:28:09 -0300

    Check items' tile to see if its FLOOR and add compatibility to regions

--- Game/src/main/java/br/usp/model/items/ItemMap.java
+++ Game/src/main/java/br/usp/model/items/ItemMap.java
@@ -4,10 +4,11 @@
  */
 package br.usp.model.items;
 
-import br.usp.model.GameObject;
 import br.usp.model.map.MapData;
+import br.usp.model.map.MapRegion;
 import br.usp.model.map.MapRegionManager;
 import br.usp.model.map.Tile;
+import br.usp.model.map.TileMap;
 import br.usp.model.map.TileType;
 import java.util.ArrayList;
 import java.util.Map;
@@ -24,8 +25,7 @@ public class ItemMap {
         this.gameItems = new ArrayList<>();
     }
 
-    //IMPLEMENTAR
-    public void loadFromData(MapData data, MapRegionManager regionManager) {
+    public void loadFromData(MapData data, TileMap map, MapRegionManager regionManager) {
         gameItems.clear();
         
         if(data.getItemData() != null) {
@@ -37,14 +37,34 @@ public class ItemMap {
                 
                 ItemType type = ItemType.valueOf(typeStr);
                 Point2d pos = new Point2d(x, y);
-
+                
+                Tile itemTile = null;
+                for(Tile t : map.getTiles()) {
+                    if(t.getPosition().getX() == x && t.getPosition().getY() == y) {
+                        itemTile = t;
+                    }
+                }
+                
+                if(!(itemTile.getType() == TileType.FLOOR)) {
+                    System.out.println("Item inserido em Tile do Tipo " + itemTile.getType() + " em " + itemTile.getPosition());
+                    System.out.println("ATENÇÃO!");
+                }
+                
+                Item currentItem = null;
                 switch (type) {
                     case KEY -> {
                         Key key = new Key(id, ItemType.KEY, pos);
                         gameItems.add(key);
+                        currentItem = key;
                     }
                     // Adicione outros tipos aqui se quiser (ex: HEART)
                 }
+                
+
+                MapRegion region = regionManager.getRegion(itemTile.getId());
+                region.addItem(currentItem);
+                
+                currentItem.setVisible(region.isUnlocked());
             }
         }
     }
